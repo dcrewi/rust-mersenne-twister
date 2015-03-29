6d33a4d20c417f6d8a05dd443d2ce9fb9ed83303
David Creswick <dcrewi@gyrae.net>
2015-03-28 23:08:19 -0500

    docs for 0.1.3

---
+++ doc/0.1.3/implementors/rand/trait.Rng.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['mersenne_twister'] = ["<a class='stability Deprecated' title='Deprecated: use the crates.io `rand` library instead'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='mersenne_twister/struct.MT19937.html' title='mersenne_twister::MT19937'>MT19937</a>","<a class='stability Deprecated' title='Deprecated: use the crates.io `rand` library instead'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/rand/trait.Rng.html' title='rand::Rng'>Rng</a> for <a class='struct' href='mersenne_twister/struct.MT19937_64.html' title='mersenne_twister::MT19937_64'>MT19937_64</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
