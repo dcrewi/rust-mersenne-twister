3fe978d8038c2a56a006bfa967c5abbac0379ba7
David Creswick <dcrewi@gyrae.net>
2015-04-13 02:41:25 -0500

    docs for 0.1.5

---
+++ doc/0.1/implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['mersenne_twister'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='mersenne_twister/struct.MT19937.html' title='mersenne_twister::MT19937'>MT19937</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='mersenne_twister/struct.MT19937_64.html' title='mersenne_twister::MT19937_64'>MT19937_64</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
