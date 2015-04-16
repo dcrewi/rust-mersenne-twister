89d2c6c80e98e53683f2d01ec8d2e4e266deb0e9
David Creswick <dcrewi@gyrae.net>
2015-04-16 14:18:21 -0500

    docs for 0.2

---
+++ doc/0.2/implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rand'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];implementors['mersenne_twister'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='mersenne_twister/struct.MT19937.html' title='mersenne_twister::MT19937'>MT19937</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='mersenne_twister/struct.MT19937_64.html' title='mersenne_twister::MT19937_64'>MT19937_64</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/0.2/mersenne_twister/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["MT19937","The 32-bit flavor of the Mersenne Twister pseudorandom number generator."],["MT19937_64","The 64-bit flavor of the Mersenne Twister pseudorandom number generator."]],"type":[["MersenneTwister","The most platform-appropriate Mersenne Twister flavor."]]});
